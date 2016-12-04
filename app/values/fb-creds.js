303d441c838ef3a27d9b8cbc7283487c3ba56511
julia kim-chung <julhkm@gmail.com>
2016-12-04 08:00:32 -0600

    Delete fb-creds.js
    accidentally pushed

--- app/values/fb-creds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-app.constant("FBCreds", {
-	key: "AIzaSyBmhM4ZkysQjzr4ihiquxF8lblQVk9AkaY",
-    authDomain: "ng-todo-59260.firebaseapp.com",
-    URL: "https://ng-todo-59260.firebaseio.com"
-});
